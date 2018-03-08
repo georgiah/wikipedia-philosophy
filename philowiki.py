ca6a30e8b7f0d9396bbdff88541f1f8f05263da3
georgiah <georgia+github@fastmail.com>
2018-03-08 15:49:49 +0100

    Updated HTML file

--- philowiki.py
+++ philowiki.py
@@ -93,91 +93,90 @@ suffixes = ['.png', '.PNG', '.svg', '.SVG', '.gif', '.GIF', '.jpg', 'JPG', '.jpe
 urlBase = "https://en.wikipedia.org"
 goal = "/wiki/Philosophy"
 
-while True:
-    # load the site dictionary
-    try:
-        sites = pickle.load(open('wikidict.p', 'rb'))
-    except:
-        sites = {}
-        pickle.dump(sites, open('wikidict.p', 'wb'))
-
-    exceptions = getExceptionSiteList()
-
-    # start with a random article
-    history = ['/wiki/Special:Random']
-
-    while validate(history):
-        # if we already know where this page links, look it up
-        try:
-            history.append(sites[history[-1]])
-            print(history[-1])
-            continue
-        # otherwise, generate a new URL
-        except:
-            url = urlBase + history[-1]
-
-        # open the URL and parse the data
-        data = urllib.urlopen(url)
-        parsedHtml = BeautifulSoup(data, "html.parser")
-
-        # find the div where the content starts and remove text in brackets
-        parsedHtml = parsedHtml.find('div', {'class': 'mw-parser-output'})
-        parsedString = parsedHtml.prettify().encode('utf-8')
-        parsedString = bracketParser(parsedString)
-
-        # parse the resulting html and find all the link
-        parsedHtml = BeautifulSoup(parsedString, "html.parser")
-        links = parsedHtml.find_all('a')
-
-        # if there aren't any links, the game's over
-        if len(links) == 0:
-            print('\nNo available links')
-            exceptionFound(history[1:])
-            break
-
-        for link in links:
-            tabled = False
-
-            # if there isn't a URL to reference, skip to the next link
-            try:
-                a = link['href']
-            except:
-                continue
-
-            # if the link isn't internal, or is an image, skip to the next link
-            if (a[:6] != '/wiki/' or any(x in a for x in suffixes) or a[:16] == '/wiki/Wikipedia:'):
-                continue
-
-            for parent in link.parents:
-                try:
-                    clss = parent['class']
-                except:
-                    clss = ''
-                try:
-                    d = parent['id']
-                except:
-                    d = ''
-
-                # if the link has a parent that signifies it isn't in the main
-                # content body, set the tabled flag and skip to the next link
-                if (parent.name == 'table' or any(x in d for x in ids) or
-                any(x in clss for x in classes)):
-                    tabled = True
-                    break
-            if tabled:
-                continue
-
-            # if the link jumps to a section, remove that section from the URL
-            pos = a.find('#')
-            if pos != -1:
-                a = a[:pos]
-
-            # add the new link to the history and update the dictionary
-            history.append(a)
-            print(history[-1])
-            if len(history) > 2:
-                sites[history[-2]] = history[-1]
-            break
-
-    # save the dictionary
+# load the site dictionary
+try:
+    sites = pickle.load(open('wikidict.p', 'rb'))
+except:
+    sites = {}
     pickle.dump(sites, open('wikidict.p', 'wb'))
+
+exceptions = getExceptionSiteList()
+
+# start with a random article
+history = ['/wiki/Special:Random']
+
+while validate(history):
+    # if we already know where this page links, look it up
+    try:
+        history.append(sites[history[-1]])
+        print(history[-1])
+        continue
+    # otherwise, generate a new URL
+    except:
+        url = urlBase + history[-1]
+
+    # open the URL and parse the data
+    data = urllib.urlopen(url)
+    parsedHtml = BeautifulSoup(data, "html.parser")
+
+    # find the div where the content starts and remove text in brackets
+    parsedHtml = parsedHtml.find('div', {'class': 'mw-parser-output'})
+    parsedString = parsedHtml.prettify().encode('utf-8')
+    parsedString = bracketParser(parsedString)
+
+    # parse the resulting html and find all the link
+    parsedHtml = BeautifulSoup(parsedString, "html.parser")
+    links = parsedHtml.find_all('a')
+
+    # if there aren't any links, the game's over
+    if len(links) == 0:
+        print('\nNo available links')
+        exceptionFound(history[1:])
+        break
+
+    for link in links:
+        tabled = False
+
+        # if there isn't a URL to reference, skip to the next link
+        try:
+            a = link['href']
+        except:
+            continue
+
+        # if the link isn't internal, or is an image, skip to the next link
+        if (a[:6] != '/wiki/' or any(x in a for x in suffixes) or a[:16] == '/wiki/Wikipedia:'):
+            continue
+
+        for parent in link.parents:
+            try:
+                clss = parent['class']
+            except:
+                clss = ''
+            try:
+                d = parent['id']
+            except:
+                d = ''
+
+            # if the link has a parent that signifies it isn't in the main
+            # content body, set the tabled flag and skip to the next link
+            if (parent.name == 'table' or any(x in d for x in ids) or
+            any(x in clss for x in classes)):
+                tabled = True
+                break
+        if tabled:
+            continue
+
+        # if the link jumps to a section, remove that section from the URL
+        pos = a.find('#')
+        if pos != -1:
+            a = a[:pos]
+
+        # add the new link to the history and update the dictionary
+        history.append(a)
+        print(history[-1])
+        if len(history) > 2:
+            sites[history[-2]] = history[-1]
+        break
+
+# save the dictionary
+pickle.dump(sites, open('wikidict.p', 'wb'))
